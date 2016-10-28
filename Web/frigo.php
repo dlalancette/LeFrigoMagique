d95720cd42b83668c69f99c48c2a383192d75d20
robinUQAC <robin.dervieux@free.fr>
2016-10-28 16:54:57 +0200

    AJout des pages web une fois loggé

---
+++ Web/frigo.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <?php require('logheader.php');?>
+
+    <body>
+           <div class="container">
+             
+                 <h1 class="text-center">Inventaire du frigo</h1>
+               <div class="row marketing">
+                  
+                     <div class="col-lg-6">
+                        <img src="img/Frigo.jpg" class="img-responsive" alt="Frigo"> 
+                    </div>
+                    <div class="col-lg-6">
+                        
+                            <div class="col-md-4">
+                                <h4>Congelé</h4>
+                                <p>- </p>
+                                <p>- </p>
+                                <p>- </p>
+                                <p>- </p>
+                            </div>
+                            <div class="col-md-4">
+                                <h4>Viande</h4>
+                                 <p>- </p>
+                                <p>- </p>
+                                <p>- </p>
+                                <p>- </p>
+                            </div> 
+                            <div class="col-md-4">
+                                <h4>Poisson</h4>
+                                 <p>- </p>
+                                <p>- </p>
+                                <p>- </p>
+                                <p>- </p>
+                            </div> 
+                            <div class="col-md-4">
+                                <h4>Produits laitier</h4>
+                                 <p>- </p>
+                                <p>- </p>
+                                <p>- </p>
+                                <p>- </p>
+                            </div> 
+                        
+                   </div>
+               </div>
+         
+            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+            <!-- Include all compiled plugins (below), or include individual files as needed -->
+            <script src="js/bootstrap.min.js"></script>
+
+            <?php require 'footer.php';?>
+        </div>
+    </body>
+    
+</html>
---
+++ Web/inventaire.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <?php require('logheader.php');?>
+
+    <body>
+           <div class="container">
+
+
+         
+            <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+            <!-- Include all compiled plugins (below), or include individual files as needed -->
+            <script src="js/bootstrap.min.js"></script>
+
+            <?php require 'footer.php';?>
+        </div>
+    </body>
+    
+</html>
